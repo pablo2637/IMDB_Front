b478ba219ee8f96d818c3eca587b8ee0719aefd6
pablo2637 <pabli2637@gmail.com>
2023-03-31 10:40:25 +0200

    mas fallos

--- middlewares/onlyAdmin.js
+++ middlewares/onlyAdmin.js
@@ -2,14 +2,14 @@ const { isAdmin } = require('../helpers/registerUser')
 
 const onlyAdmin = async (req, res, next) => {
     const rol = await isAdmin(req, res)
-    console.log('onlyAdmin', rol)
+    // console.log('onlyAdmin', rol)
     if (rol == 'admin') next();
     else res.redirect('/dashboard/')
 }
 
 const notAdmin = async (req, res, next) => {
     const rol = await isAdmin(req, res)
-    console.log('notAdmin', rol)
+    // console.log('notAdmin', rol)
     if (rol != 'admin') next();
     else res.redirect('/dashboard-admin/')
 }
